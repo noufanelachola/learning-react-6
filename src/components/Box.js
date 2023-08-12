d1fb6b6566b4002d241c008e5a146fb895a444d4
noufanelachola <noufansadiqelachola@gmail.com>
2023-08-12 17:50:36 +0530

    comnpleeeeeee

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,18 +1,13 @@
 import React , {useState} from "react";
 
 function Box(props) {
-
-    const [on , setOn] = useState(props.on)
-
     const styles = {
-        backgroundColor: on ? "#222222" : "#cccccc"
-    }
+        backgroundColor: props.on ? "#222222" : "#cccccc"
+    };
 
-    return <div style={styles} className="box" onClick={toggle}></div>
+    return <div style={styles} className="box" onClick={() => props.handleClick(props.id)}></div>
     
-    function toggle() {
-        setOn(prevOn => !prevOn);
-    }
+
 }
 
 export default Box;
--- src/containers/App.js
+++ src/containers/App.js
@@ -6,9 +6,41 @@ function App() {
 
   const [squares , setSquares ] = useState(Boxes);
 
+  
+  // function toggle(id){
+    
+  //     setSquares( prevSquares => {
+  //       const newSquares = [];
+  //       for( let i = 0 ; i < prevSquares.length ; i++){
+  //         const currentSquare = prevSquares[i];
+          
+  //         if(currentSquare.id === id ){
+  //           const updatedSquare = {
+  //             ...currentSquare,
+  //             on: !currentSquare.on
+  //           }
+  //           newSquares.push(updatedSquare);
+  //         } else {
+  //           newSquares.push(currentSquare);
+  //         }
+  //       }
+  //       return newSquares;
+  //     })
+  
+    
+  // }
+
+
+  function toggle(id){
+    setSquares(prevSquares => {
+      return prevSquares.map(square => {
+        return square.id === id ? {...square, on:!square.on} : square ;
+      })
+    })
+  }
 
   const newSquares = squares.map(square => {
-    return <Box key={square.id} id={square.id} on={square.on} />
+    return <Box key={square.id} id={square.id} on={square.on} handleClick={toggle}/>
   })
 
   return (
